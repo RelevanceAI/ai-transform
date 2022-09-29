47516ef9856f603e1c9e0a26de3e3c295bff9df5
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-09-29 11:39:48 +1000

    more tests

--- tests/test_dataset/test_dataset.py
+++ tests/test_dataset/test_dataset.py
@@ -4,6 +4,13 @@ from slim.utils import mock_documents
 
 
 class TestDataset:
+    def test_schema(self, test_dataset: Dataset):
+        documents = mock_documents(100)
+        keys = documents[0].keys()
+        test_dataset.insert_documents(documents)
+        schema = test_dataset.schema
+        assert all([key in schema for key in keys])
+
     def test_insert(self, test_dataset: Dataset):
         documents = mock_documents(100)
         result = test_dataset.insert_documents(documents)
