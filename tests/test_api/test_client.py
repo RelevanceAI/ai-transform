003244b296f1dc58ab0f5eda36af497e615146b4
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-09-29 10:30:27 +1000

    tests

--- tests/test_api/test_client.py
+++ tests/test_api/test_client.py
@@ -2,6 +2,6 @@ from slim import Client
 
 
 class TestClient:
-    def test_create_dataset(test_client: Client):
+    def test_create_dataset(self, test_client: Client):
         res = test_client.create_dataset("test_dataset")
         assert True
