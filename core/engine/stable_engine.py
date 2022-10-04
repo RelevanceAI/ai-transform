5d2c4a056bfcc128ff452e439fb23f8c03b7938a
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-10-04 15:56:24 +1000

    progress bar

--- core/engine/stable_engine.py
+++ core/engine/stable_engine.py
@@ -2,12 +2,19 @@ from typing import Any
 
 from core.engine.abstract_engine import AbstractEngine
 
+from tqdm.auto import tqdm
+
 
 class StableEngine(AbstractEngine):
+    def __init__(self, show_progress_bar: bool = True, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._show_progress_bar = show_progress_bar
+
     def apply(self) -> Any:
 
         iterator = self.iterate()
-        for chunk in iterator:
+        for chunk in tqdm(iterator, disable=(not self._show_progress_bar)):
             new_batch = self.operator(chunk)
             self.update_chunk(new_batch)
 
