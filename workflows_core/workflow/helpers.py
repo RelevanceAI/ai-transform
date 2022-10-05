42158136ca66b920d9d247c53040660a2478111b
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-10-05 12:57:00 +1000

    read_token_from_sript

--- workflows_core/workflow/helpers.py
+++ workflows_core/workflow/helpers.py
@@ -1,6 +1,7 @@
 """
 Base64 decoding for workflows
 """
+import argparse
 import os
 import base64
 import json
@@ -25,4 +26,17 @@ def decode_workflow_token(token: str) -> Mapping[str, Any]:
     config = json.loads(base64.b64decode(token + "==="))
     # Set workflow ID for tracking
     os.environ["WORKFLOW_ID"] = config.get("job_id", "")
+    return config
+
+
+def read_token_from_script():
+    """
+    Reads in a token from script and returns a config as a
+    dictionary object.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("token", help="The token used for the workflow config.")
+    args = parser.parse_args()
+    token = args.token
+    config = decode_workflow_token(token)
     return config
