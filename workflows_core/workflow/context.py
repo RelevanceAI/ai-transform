a9586cb861c3891a0c2087a79ac39d2954514218
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-10-07 10:46:49 +1000

    enter exit

---
+++ workflows_core/workflow/context.py
@@ -0,0 +1,9 @@
+class WorkflowContext:
+    def __init__(self) -> None:
+        pass
+
+    def __enter__(self, *args, **kwargs):
+        pass
+
+    def __exit__(self, *args, **kwargs):
+        pass
