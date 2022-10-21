497105edc98ab00bbc637cd657d3085cd328c00f
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-10-21 11:32:14 +1000

    add tests

---
+++ tests/core/test_utils/test_documents.py
@@ -0,0 +1,27 @@
+from copy import deepcopy
+
+from workflows_core.utils.documents import Documents
+
+
+class TestDocuments:
+    def test_deepcopy(self, test_documents: Documents):
+        copy_test_documents = deepcopy(test_documents)
+        copy_test_documents["field3"] = 4
+        copy_test_documents["field1.field2"] = 4
+
+        assert all(
+            test_document.get("field3") != copy_test_document["field3"]
+            for test_document, copy_test_document in zip(
+                test_documents, copy_test_documents
+            )
+        )
+        assert all(
+            test_document.get("field1.field") != copy_test_document["field1.field2"]
+            for test_document, copy_test_document in zip(
+                test_documents, copy_test_documents
+            )
+        )
+
+    def test_serializer(self, test_documents: Documents):
+        serialized = test_documents.serialize()
+        assert True
