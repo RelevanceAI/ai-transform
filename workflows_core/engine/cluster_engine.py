3b06a858994a3682be3a589e3e35276f3219f8ac
jacky2wong <jacky2wong@gmail.com>
2022-11-01 16:27:10 +1100

    add workflows core engine

--- workflows_core/engine/cluster_engine.py
+++ workflows_core/engine/cluster_engine.py
@@ -2,6 +2,7 @@ import concurrent.futures
 
 import logging
 import traceback
+import time
 from typing import Any
 
 from workflows_core.engine.abstract_engine import AbstractEngine
@@ -47,22 +48,29 @@ class InMemoryEngine(AbstractEngine):
         else:
             self._success_ratio = 1.0
 
-        def payload_generator():
-            for i in range(self._num_chunks):
-                yield new_batch[i * self._chunksize : (i + 1) * self._chunksize]
-                self._progress.update(1)
+        # Update this in series
+        for i in range(self._num_chunks):
+            chunk = new_batch[i * self.chunksize : (i + 1) * self._chunksize]
+            self.update_chunk(chunk, ingest_in_background=True)
+            time.sleep(0.5)
 
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [
-                executor.submit(self.update_chunk, payload)
-                for payload in payload_generator()
-            ]
+        # WE have to remove this code to avoid hammering the server
+        # def payload_generator():
+        #     for i in range(self._num_chunks):
+        #         yield new_batch[i * self._chunksize : (i + 1) * self._chunksize]
+        #         self._progress.update(1)
 
-            for future in concurrent.futures.as_completed(futures):
-                try:
-                    result = future.result()
-                except Exception as e:
-                    logging.error(e)
-                    logging.error(traceback.format_exc())
-                else:
-                    logging.debug(result)
+        # with concurrent.futures.ThreadPoolExecutor() as executor:
+        #     futures = [
+        #         executor.submit(self.update_chunk, payload)
+        #         for payload in payload_generator()
+        #     ]
+
+        #     for future in concurrent.futures.as_completed(futures):
+        #         try:
+        #             result = future.result()
+        #         except Exception as e:
+        #             logging.error(e)
+        #             logging.error(traceback.format_exc())
+        #         else:
+        #             logging.debug(result)
