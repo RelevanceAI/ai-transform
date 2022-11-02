096760111d508110b09183e360f7a64dfd160efa
jacky2wong <jacky2wong@gmail.com>
2022-11-03 10:39:06 +1100

    add a script to create a token and a random example

---
+++ examples/workflows/create_token.py
@@ -0,0 +1,12 @@
+import os
+from workflows_core.utils import encode_parameters
+
+token = encode_parameters(
+    parameters={
+        "numeric_field": "rating",
+        "dataset_id": "aaa-test",
+        "authorizationToken": os.environ["TEST_TOKEN"]
+    }
+)
+
+print(token)
---
+++ examples/workflows/random_example.py
@@ -0,0 +1,92 @@
+"""
+An example showing how to write a simple workflow. 
+In this workflow, we add one to every number.
+"""
+# For this workflow to to run, you need the following;
+# transformers[torch]==4.18.0
+# relevance-workflows-core
+
+import uuid
+import random
+from typing import Callable, List, Optional
+
+from workflows_core.api.client import Client
+from workflows_core.engine.stable_engine import StableEngine
+from workflows_core.workflow.helpers import decode_workflow_token
+from workflows_core.workflow.abstract_workflow import AbstractWorkflow
+from workflows_core.operator.abstract_operator import AbstractOperator
+from workflows_core.utils.document_list import DocumentList
+
+
+class RandomOperator(AbstractOperator):
+    def __init__(
+        self,
+        numeric_field: str,
+    ):
+        self.numeric_field = numeric_field
+
+        super().__init__(
+            input_fields=[
+                numeric_field
+            ],
+            output_fields=[
+                f"_random_.{numeric_field}"
+            ],
+        )
+
+    def transform(self, documents: DocumentList) -> DocumentList:
+        """
+        Main transform function
+        """
+        for d in documents:
+            # add one to the numeric field
+            d[self.numeric_field] = d[self.numeric_field] + 1
+        return documents
+
+def execute(token: str, logger: Callable, worker_number: int = None, *args, **kwargs):
+    config = decode_workflow_token(token)
+
+    job_id = config.get("job_id", str(uuid.uuid4()))
+    token = config["authorizationToken"]
+    dataset_id = config["dataset_id"]
+    numeric_field = config["numeric_field"]
+    total_workers = config.get("total_workers", None)
+
+    client = Client(token=token)
+    dataset = client.Dataset(dataset_id)
+
+    operator = RandomOperator(
+        numeric_field=numeric_field
+    )
+
+    filters = dataset[numeric_field].exists()
+
+    engine = StableEngine(
+        dataset=dataset,
+        operator=operator,
+        chunksize=8,
+        select_fields=[numeric_field],
+        filters=filters,
+        worker_number=worker_number,
+        total_workers=total_workers,
+    )
+
+    workflow = AbstractWorkflow(
+        engine=engine,
+        job_id=job_id,
+        name="AddOne"
+    )
+    workflow.run()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Sentiment workflow.")
+    parser.add_argument(
+        "token",
+        type=str,
+        help="a base64 encoded token that contains parameters for running the workflow",
+    )
+    args = parser.parse_args()
+    execute(args.token, print)
