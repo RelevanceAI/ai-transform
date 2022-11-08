de48edf5affe5f3a1e31d6322aca662fe9ce0405
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-11-08 13:38:12 +1000

    added extra test

--- tests/core/test_operator/test_document_diff.py
+++ tests/core/test_operator/test_document_diff.py
@@ -1,8 +1,9 @@
-from copy import deepcopy
 import json
 import random
 
+from copy import deepcopy
 from workflows_core.operator.abstract_operator import AbstractOperator
+from workflows_core.utils.example_documents import mock_documents
 
 
 class TestDocumentDiff:
@@ -18,3 +19,8 @@ class TestDocumentDiff:
 
         expected = json.dumps({"label": "yes"})
         assert all(json.dumps(document.to_json()) == expected for document in diff)
+
+    def test_no_diff(self):
+        documents = mock_documents()
+        diff = AbstractOperator._postprocess(documents, documents)
+        assert all(json.dumps(document_diff) == str({}) for document_diff in diff)
