4b37e2db7903e4c7d0b562dee15f909f97de0f8a
jacky2wong <jacky2wong@gmail.com>
2022-11-11 17:31:24 +1100

    add workflows config

--- workflows_core/config.py
+++ workflows_core/config.py
@@ -1,6 +1,7 @@
 """
 Config class
 """
+import argparse
 from pydantic import BaseModel
 from pydantic.schema import schema
 
@@ -8,3 +9,16 @@ class BaseConfig(BaseModel):
     @classmethod
     def to_schema(self):
         return self.schema_json()
+    
+    def read_from_argparser(self, argparser: argparse.ArgumentParser):
+        # Enables behavior such
+        # reads in required attributes from argparser
+        for k in self.dict():
+            # gets the required attributes like 'text_field'
+            setattr(self, k, getattr(argparser, k))
+        
+    def get(self, value, default=None):
+        if hasattr(self, value):
+            return getattr(self, value)
+        else:
+            return default
