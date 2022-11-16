0fc7995e00bd67d801c1ee4caa6804400f264047
Jacky Wong <jacky.wong@relevance.ai>
2022-11-17 09:57:51 +1100

    update pandas

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import find_packages, setup
 
 from workflows_core import __version__
 
-requirements = ["tqdm>=4.49.0", "requests>=2.0.0", "pydantic==1.10.2"]
+requirements = ["tqdm>=4.49.0", "requests>=2.0.0", "pydantic==1.10.2", "pandas>=1.0.0"]
 
 ray_requirements = [
     "numpy>=1.19.0",
     "pyarrow==9.0.0",
     "ray==2.0.0",
-    "pandas>=1.0.0",
 ]
 
 core_test_requirements = [
