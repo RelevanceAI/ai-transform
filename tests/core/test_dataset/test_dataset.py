27c9ca12ebb190114f2797c334be22662f87263e
yifangs <yifang.bill.sun@gmail.com>
2022-11-18 12:24:28 +1100

    made changes and add test

--- tests/core/test_dataset/test_dataset.py
+++ tests/core/test_dataset/test_dataset.py
@@ -142,20 +142,23 @@ class TestKeyphraseCrud:
 
         keyphrase_field.update_keyphrases(keyphrases_update={"cat": {"count": 5, "sentiment_score": 0.8, "goodness_score": 1.2},
                                                              "pig": {"count": 4, "sentiment_score": 0.7, "goodness_score": 1.1}
-                                                             })
-        keyphrases = keyphrase_field.get_keyphrases()
+                                                             },
+                                          alias='default')
+        keyphrases = keyphrase_field.get_keyphrases(alias='default')
         assert "cat" in keyphrases and "pig" in keyphrases
 
         keyphrase_field.insert_keyphrases(keyphrases_insert={"cat": {"count": 5, "sentiment_score": 0.7, "goodness_score": 1.2},
                                                              "duck": {"count": 6, "sentiment_score": 0.9, "goodness_score": 1.5}
-                                                             })
-        keyphrases = keyphrase_field.get_keyphrases()
+                                                             },
+                                          alias='default')
+        keyphrases = keyphrase_field.get_keyphrases(alias='default')
         assert "cat" in keyphrases and "pig" in keyphrases and "duck" in keyphrases
         assert keyphrases['cat']['count'] == 10
         assert keyphrases['cat']['sentiment_score'] == 0.7
 
         keyphrase_field.remove_keyphrases(keyphrases_remove={"cat": {"count": 5, "sentiment_score": 0.8, "goodness_score": 1.2},
                                                              "chicken": {"count": 4, "sentiment_score": 0.7, "goodness_score": 1.1}
-                                                             })
-        keyphrases = keyphrase_field.get_keyphrases()
+                                                             },
+                                          alias='default')
+        keyphrases = keyphrase_field.get_keyphrases(alias='default')
         assert "cat" not in keyphrases and "pig" in keyphrases and "duck" in keyphrases and "chicken" not in keyphrases
