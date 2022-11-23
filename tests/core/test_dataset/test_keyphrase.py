c16bd169fa31a0f286ad14975c6d1f3a6035ea0e
yifangs <yifang.bill.sun@gmail.com>
2022-11-23 17:23:58 +1100

    update to new endpoints

--- tests/core/test_dataset/test_keyphrase.py
+++ tests/core/test_dataset/test_keyphrase.py
@@ -19,28 +19,3 @@ class TestKeyphraseCrud:
 
         keyphrase = keyphrase_field.get_keyphrase(alias='default', keyphrase_id="cat")
         assert "cat" == keyphrase['text']
-
-        keyphrase_field.delete_keyphrase(alias="default", keyphrase_id="cat")
-        keyphrases = keyphrase_field.list_keyphrases(alias="default")
-        print(keyphrases)
-        has_cat = False
-        for keyphrase in keyphrases:
-            if "cat" == keyphrase["text"]:
-                has_cat = True
-        assert not has_cat
-
-        # update_keyphrase().insert_keyphrases(keyphrases_insert={"cat": {"count": 5, "sentiment_score": 0.7, "goodness_score": 1.2},
-        #                                                      "duck": {"count": 6, "sentiment_score": 0.9, "goodness_score": 1.5}
-        #                                                      },
-        #                                       alias='default')
-        # keyphrases = keyphrase_field.get_keyphrases(alias='default')
-        # assert "cat" in keyphrases and "pig" in keyphrases and "duck" in keyphrases
-        # assert keyphrases['cat']['count'] == 10
-        # assert keyphrases['cat']['sentiment_score'] == 0.7
-        #
-        # keyphrase_field.remove_keyphrases(keyphrases_remove={"cat": {"count": 5, "sentiment_score": 0.8, "goodness_score": 1.2},
-        #                                                      "chicken": {"count": 4, "sentiment_score": 0.7, "goodness_score": 1.1}
-        #                                                      },
-        #                                   alias='default')
-        # keyphrases = keyphrase_field.get_keyphrases(alias='default')
-        # assert "cat" not in keyphrases and "pig" in keyphrases and "duck" in keyphrases and "chicken" not in keyphrases
