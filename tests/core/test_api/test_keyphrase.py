df2091250b014589ef0bd9a93c8fe5fefa165c46
Jacky Wong <jacky.wong@relevance.ai>
2022-12-23 12:49:54 +1100

    print result

--- tests/core/test_api/test_keyphrase.py
+++ tests/core/test_api/test_keyphrase.py
@@ -36,4 +36,5 @@ class TestClient:
         result = test_client._api._get_keyphrase(
             test_dataset_id, field=field, alias=alias, keyphrase_id=keyphrase_id
         )
+        print(result)
         assert result["text"] == "word" and result["score"] == 10, result
