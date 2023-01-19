00d315af7d2ac7d34818babeca41173d8c6d3500
jtwinrelevanceai <joseph.twin@relevance.ai>
2023-01-20 09:50:07 +1000

    black tests

--- tests/core/test_dataset/test_keyphrase.py
+++ tests/core/test_dataset/test_keyphrase.py
@@ -9,10 +9,26 @@ class TestKeyphraseCrud:
     def test_crud(self, full_dataset: Dataset):
         keyphrase_field = full_dataset["_keyphrase_.sample_1_label.default"]
 
-        keyphrase1 = Keyphrase(text="cat", _id="cat", ancestors=[], parents=[], level=0, keyphrase_score=1.1,
-                               frequency=2, metadata={})
-        keyphrase2 = Keyphrase(text="cat", _id="dog", ancestors=["cat"], parents=["cat"],
-                               level=1, keyphrase_score=1.1, frequency=1, metadata={})
+        keyphrase1 = Keyphrase(
+            text="cat",
+            _id="cat",
+            ancestors=[],
+            parents=[],
+            level=0,
+            keyphrase_score=1.1,
+            frequency=2,
+            metadata={},
+        )
+        keyphrase2 = Keyphrase(
+            text="cat",
+            _id="dog",
+            ancestors=["cat"],
+            parents=["cat"],
+            level=1,
+            keyphrase_score=1.1,
+            frequency=1,
+            metadata={},
+        )
         updates = [keyphrase1, keyphrase2]
         response = keyphrase_field.bulk_update_keyphrases(
             updates=updates,
