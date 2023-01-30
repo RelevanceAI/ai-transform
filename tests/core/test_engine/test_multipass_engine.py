c8ecfca83e8e3fef4d865476a50e2e8ac7670060
jtwinrelevanceai <joseph.twin@relevance.ai>
2023-01-30 17:24:46 +1000

    fix tests

--- tests/core/test_engine/test_multipass_engine.py
+++ tests/core/test_engine/test_multipass_engine.py
@@ -1,6 +1,5 @@
 from workflows_core.dataset.dataset import Dataset
-from workflows_core.engine.stable_engine import StableEngine
-from workflows_core.engine.small_batch_stable_engine import SmallBatchStableEngine
+from workflows_core.engine.multipass_engine import MultiPassEngine
 
 from workflows_core.operator.abstract_operator import AbstractOperator
 from workflows_core.workflow.abstract_workflow import AbstractWorkflow
@@ -10,7 +9,7 @@ class TestStableEngine:
     def test_multipass_engine_abstract(
         self, full_dataset: Dataset, test_operator: AbstractOperator
     ):
-        engine = StableEngine(
+        engine = MultiPassEngine(
             dataset=full_dataset,
             operators=[
                 test_operator,
