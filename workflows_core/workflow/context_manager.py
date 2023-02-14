fe996984b4d49cadc0891d1655abb37a358220ae
Jacky Wong <jacky.wong@relevance.ai>
2023-02-14 13:41:05 +1100

    hotfix priicng

--- workflows_core/workflow/context_manager.py
+++ workflows_core/workflow/context_manager.py
@@ -89,27 +89,13 @@ class WorkflowContextManager(API):
         return
 
     def __exit__(self, exc_type: type, exc_value: BaseException, traceback: Traceback):
+        self._calculate_pricing()
         if exc_type is not None:
             self._set_status(
                 status=self.FAILED, worker_number=self._engine.worker_number
             )
 
             logger.exception(exc_value)
-
-            n_processed_pricing = 0
-            for operator in self._operators:
-                n_processed_pricing += operator.n_processed_pricing
-
-            # Set default pricing to the the number of documents
-            if n_processed_pricing == 0:
-                n_processed_pricing = self._engine.size
-
-            self._dataset.api._update_workflow_pricing(
-                workflow_id=self._job_id,
-                step=self._workflow_name,
-                worker_number=self._engine.worker_number,
-                n_processed_pricing=n_processed_pricing,
-            )
 
             self._update_workflow_metadata(
                 job_id=self._job_id,
@@ -140,6 +126,23 @@ class WorkflowContextManager(API):
                     output=self._engine.output_documents,
                 )
             return True
+        
+    def _calculate_pricing(self):
+        n_processed_pricing = 0
+        for operator in self._operators:
+            n_processed_pricing += operator.n_processed_pricing
+
+        # Set default pricing to the the number of documents
+        if n_processed_pricing == 0:
+            n_processed_pricing = self._engine.size
+
+        self._dataset.api._update_workflow_pricing(
+            workflow_id=self._job_id,
+            step=self._workflow_name,
+            worker_number=self._engine.worker_number,
+            n_processed_pricing=n_processed_pricing,
+        )
+
 
     def set_field_children(self, input_field: str, output_fields: list):
         # Implement the config ID and authorization token
