935494db0edba0decf312245ad997a6b3aae5c2f
Jacky Wong <jacky.wong@relevance.ai>
2023-02-20 21:57:37 +1100

    add safe divide

--- ai_transform/engine/abstract_engine.py
+++ ai_transform/engine/abstract_engine.py
@@ -418,9 +418,13 @@ class AbstractEngine(ABC):
         self._name = value
 
     def set_success_ratio(self) -> None:
-        self._success_ratio = self._successful_documents / (
-            self.size * len(self.operators)
-        )
+        try:
+            self._success_ratio = self._successful_documents / (
+                self.size * len(self.operators)
+            )
+        except ZeroDivisionError:
+            # if theres no documents just set success ratio at 1
+            self._success_ratio = 1
         logger.debug(format_logging_info({"success_ratio": self._success_ratio}))
 
     @staticmethod
