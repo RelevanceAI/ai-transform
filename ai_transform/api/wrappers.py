0a0596d4f514480ef8480841afa932afea3c73f3
jtwinrelevanceai <joseph.twin@relevance.ai>
2023-03-22 08:09:35 +1000

    add update tests

--- ai_transform/api/wrappers.py
+++ ai_transform/api/wrappers.py
@@ -1,7 +1,15 @@
 import time
+import logging
 import requests
 
+from requests.models import Response
+
+from concurrent.futures import ThreadPoolExecutor
+
 from typing import Union, Sequence, Mapping, Any
+
+logger = logging.getLogger(__file__)
+logging.basicConfig()
 
 
 def request_wrapper(
@@ -10,13 +18,26 @@ def request_wrapper(
     kwargs: Mapping[str, Any] = None,
     num_retries: int = 3,
     timeout: int = 30,
-) -> requests.Request:
+    raise_errors: bool = False,
+) -> Response:
+
+    if args is None:
+        args = ()
+
+    if kwargs is None:
+        kwargs = {}
+
     for _ in range(num_retries):
         try:
             result = fn(*args, **kwargs)
-            result.json()
-        except:
+            assert result.status_code == 200
+        except Exception as e:
+            logger.exception(e)
+            if raise_errors:
+                raise e
             time.sleep(timeout)
         else:
             return result
-    raise ValueError(f"Request was not able to be completed within {num_retries}")
+    raise ValueError(
+        f"Request was not able to be completed within {num_retries} retries"
+    )
