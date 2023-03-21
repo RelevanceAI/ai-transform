1038a4c3ea24007fbce0b88d507a83d7e2441783
jtwinrelevanceai <joseph.twin@relevance.ai>
2023-03-21 12:40:01 +1000

    update fn

--- ai_transform/api/wrappers.py
+++ ai_transform/api/wrappers.py
@@ -1,3 +1,6 @@
+import time
+import requests
+
 from typing import Callable, Sequence, Mapping, Any
 
 
@@ -5,6 +8,17 @@ def openai_wrapper(
     fn: Callable,
     args: Sequence = None,
     kwargs: Mapping[str, Any] = None,
-):
-    result = fn(*args, **kwargs)
-    return result
+    num_retries: int = 3,
+) -> requests.Request:
+    for _ in range(3):
+        try:
+            result = fn(*args, **kwargs)
+            try:
+                result.json()
+            except AttributeError:
+                return result
+        except:
+            time.sleep(30)
+        else:
+            return result
+    raise ValueError(f"Request was not able to be completed within {num_retries}")
