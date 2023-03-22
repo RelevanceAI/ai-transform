2d78edc9caabc8ea2b735c5d66fc7b3c80bb3ef4
Jacky Wong <jacky.wong@relevance.ai>
2023-03-23 10:45:58 +1100

    Fix args

--- tests/core/test_api/test_wrappers.py
+++ tests/core/test_api/test_wrappers.py
@@ -29,6 +29,32 @@ class TestWrappers:
         assert "status_code" in str(out) + str(err)
         assert "message" in str(out) + str(err)
 
+    def test_request_wrapper_fail(self):
+        # ####
+        f = io.StringIO()
+        u = io.StringIO()
+
+        with redirect_stdout(u):
+            try:
+                resp = request_wrapper(
+                    requests.post, 
+                    args=("https://www.google.com",),
+                    kwargs={
+                        "value": 10
+                    },
+                    num_retries=1,
+                    timeout=1,
+                    raise_errors=True
+                )
+            except Exception as e:
+                print(e)
+                pass
+        
+        out = f.getvalue()
+        err = u.getvalue()
+        assert "status_code" in str(out) + str(err)
+        assert "message" in str(out) + str(err)
+
     def test_request_wrapper_pass(self):
         resp = request_wrapper(
             requests.get, ("https://www.google.com",), timeout=1, num_retries=2
