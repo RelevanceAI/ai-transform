9184eb43444b32b39defe8ff12c8ffc143c08404
jtwinrelevanceai <joseph.twin@relevance.ai>
2023-03-22 10:39:32 +1000

    fix tests

--- tests/core/test_api/test_endpoints.py
+++ tests/core/test_api/test_endpoints.py
@@ -3,6 +3,7 @@ from ai_transform.api.client import Client
 
 class TestEndpoints:
     def test_generic_request(self, test_client: Client):
-        test_client.api.post("/auth/users/list")
-        test_client.api.get("/health")
-        assert True
+        resp = test_client.api.post("/auth/users/list")
+        resp = test_client.api.get("/health")
+        assert resp.status_code == 200
+        assert resp.status_code == 200
