a2110d8a220691954f6c0e499734796c26b527ac
jtwinrelevanceai <joseph.twin@relevance.ai>
2023-04-14 10:41:44 +1000

    add test

--- tests/core/test_engine/test_engine.py
+++ tests/core/test_engine/test_engine.py
@@ -1,8 +1,11 @@
 from typing import Any
 
+from ai_transform.api.client import Client
 from ai_transform.dataset.dataset import Dataset
 from ai_transform.operator.abstract_operator import AbstractOperator
 from ai_transform.engine.abstract_engine import AbstractEngine
+
+from ai_transform.utils.example_documents import mock_documents
 
 
 class TestAbstractEngine:
@@ -38,3 +41,15 @@ class TestAbstractEngine:
 
         engine = ExampleEngine(full_dataset, test_operator, select_fields=["_id", "_chunk_.label"])
         assert "_chunk_" in engine._select_fields
+
+    def test_engine_select_fields(self, test_client: Client, test_dataset_id: str, test_operator: AbstractOperator):
+        class ExampleEngine(AbstractEngine):
+            def apply(self) -> Any:
+                return
+
+        engine = ExampleEngine(test_client.Dataset(test_dataset_id), test_operator, documents=mock_documents(20))
+        engine()
+
+        assert len(engine.output_documents) == 20
+
+        test_client.delete_dataset(test_dataset_id)
