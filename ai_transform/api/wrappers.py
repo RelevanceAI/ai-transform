d1ed273b638f82f8a68ea3bcd7a2d7e8fc4a28d5
jtwinrelevanceai <joseph.twin@relevance.ai>
2023-04-14 10:58:46 +1000

    refactor to function:

--- ai_transform/api/wrappers.py
+++ ai_transform/api/wrappers.py
@@ -16,6 +16,29 @@ class ManualRetry(Exception):
     pass
 
 
+def is_response_bad(
+    result: Response,
+    key_for_error: str = None,
+    output_to_stdout: bool = False,  # support output to stdout to ensure logging is working
+):
+    try:
+        json_response = result.json()
+        if key_for_error in json_response:
+            raise KeyError
+
+    except JSONDecodeError:
+        error_message = "Response is not JSON decodable"
+        if output_to_stdout:
+            print(error_message)
+        raise JSONDecodeError(error_message)
+
+    except KeyError:
+        error_message = f"{key_for_error} not in JSON response"
+        if output_to_stdout:
+            print(error_message)
+        raise KeyError(error_message)
+
+
 def request_wrapper(
     fn: Union[requests.get, requests.post],
     args: Sequence = None,
@@ -57,20 +80,7 @@ def request_wrapper(
                 raise ManualRetry(to_log_for_retry)
 
             if is_json_decodable or key_for_error:
-                try:
-                    json_response = result.json()
-                    if key_for_error in json_response:
-                        raise KeyError
-                except JSONDecodeError:
-                    error_message = "Response is not JSON decodable"
-                    if output_to_stdout:
-                        print(error_message)
-                    raise JSONDecodeError(error_message)
-                except KeyError:
-                    error_message = f"{key_for_error} not in JSON response"
-                    if output_to_stdout:
-                        print(error_message)
-                    raise KeyError(error_message)
+                is_response_bad(result=result, key_for_error=key_for_error, output_to_stdout=output_to_stdout)
 
         except (Exception, ManualRetry, JSONDecodeError, KeyError) as e:
             logger.exception(e)
